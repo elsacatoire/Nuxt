d5897c87afd4f55bab435ae6dff52446a0653a76
Elsa Catoire <elsa.catoire@gmail.com>
2024-03-17 19:43:21 +0100

    handeling client error and config meta data + material icon

--- justone/nuxt.config.ts
+++ justone/nuxt.config.ts
@@ -1,5 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss']
+  modules: ['@nuxtjs/tailwindcss'],
+  app: {
+    head: {
+      title: 'OneGame',
+      meta: [
+        { name: 'description', content: 'Small board games numerized for your plasure!' }
+      ],
+      link: [
+        { rel: 'stylesheet', href: 'https://fonts.googleapis.com/icon?family=Material+Icons' }
+      ]
+    }
+  }
 })
--- justone/pages/games/[id].vue
+++ justone/pages/games/[id].vue
@@ -13,7 +13,7 @@ const uri = 'https://fakestoreapi.com/products/' + id
 const { data: product } = await useFetch(uri);
 
 if (!product.value) {
-    throw createError({ statusCode: 404, statusMessage: 'Product not found'})
+    throw createError({ statusCode: 404, statusMessage: 'Product not found', fatal: true})
 }
 
 definePageMeta({
